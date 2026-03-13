fd6feb9c7257c9cb6e144f711493adb7c7527f3d
praveenkumar-834 <praveenuu34@gmail.com>
2026-03-13 14:12:23 +0530

    db.js changed

--- config/db.js
+++ config/db.js
@@ -5,7 +5,8 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected");
   } catch (error) {
-    console.log(error);
+    console.error("DB connection error:", error);
+    process.exit(1);
   }
 };
 
